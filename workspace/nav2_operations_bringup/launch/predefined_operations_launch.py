cd67a4016b8a4c6989795b92ab5a203f2faddef3
d.kotov <bilzar@bk.ru>
2026-04-23 02:03:00 +0300

    update: default bt xml fix

--- workspace/nav2_operations_bringup/launch/predefined_operations_launch.py
+++ workspace/nav2_operations_bringup/launch/predefined_operations_launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
 from nav2_common.launch import RewrittenYaml
 
 
@@ -17,10 +18,12 @@ def generate_launch_description():
     param_substitutions = {
         'use_sim_time': use_sim_time,
     }
-    configured_params = RewrittenYaml(
-        source_file=params_file,
-        param_rewrites=param_substitutions,
-        convert_types=True)
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            param_rewrites=param_substitutions,
+            convert_types=True),
+        allow_substs=True)
 
     return LaunchDescription([
         DeclareLaunchArgument(
